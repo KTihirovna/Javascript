fcf87a1c6b93e5f7f6b4e00256f1ba32d0172828
KTohirovna <kamolaxudoyqulova17307@gmail.com>
2024-09-19 01:39:00 +0500

    Update tasks2.js

--- tasks2.js
+++ tasks2.js
@@ -130,9 +130,13 @@ let challenge = '30 Days Of JavaScript'
     console.log(numBtnZeroAndHundred);
 
 // 10. Generate a random number between 50 and 100 inclusively.
-function getRandomNumberFifty() {
-    return Math.floor(Math.random() * 101);
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+const randomNumber50To100 = getRandomNumber(50, 100);
+console.log(randomNumber50To100);
+
 
 const randomNumber5 = getRandomNumberFifty();
 console.log(randomNumber5);
@@ -153,8 +157,12 @@ console.log(randomNum);
 3 1 3 9 27
 4 1 4 16 64
 5 1 5 25 125*/
+  
+  
 
-
+for (let k = 1; k <=5; k++) {
+    console.log(`${k}\t ${1}\t ${k}\t ${k * k}\t ${k * k * k} `);
+}
 // 14. Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
     console.log(rule.substr(31,24))
 
@@ -170,15 +178,11 @@ console.log(randomNum);
     const becauseCount = (sentenceBecause.match(/because/gi) || []).length; // Use /gi to match case-insensitively
     console.log(becauseCount); // Output: 3
     
-//3. Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+// 4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
 
-    const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
-
-    // 4. Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
-
-    const incomeText = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.';
-const monthlyIncome = incomeText.match(/\d+/g).map(Number);
-
+const monthlySalary = parseInt(text.match(/(\d+) euro from salary per month/)[1]);
+const annualBonus = parseInt(text.match(/(\d+) euro annual bonus/)[1]);
+const monthlyCourses = parseInt(text.match(/(\d+) euro online courses per month/)[1]);
 // Calculate total annual income
 const totalAnnualIncome = (monthlyIncome[0] * 12) + monthlyIncome[1]; 
 console.log(totalAnnualIncome); 
